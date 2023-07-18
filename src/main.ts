611d21d5e92144b65a8c356b4cde6c5e0702a64f
Hoang Nguyen Huy <ngxhuyhoang@gmail.com>
2023-07-18 22:44:37 +0700

    feat: add some config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('E Commerce API Docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000, () => {
+    console.info('Server is running on http://localhost:3000');
+    console.info('Swagger docs http://localhost:3000/docs');
+  });
 }
 bootstrap();
