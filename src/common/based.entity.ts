5e5f44fbc38f66426bbc9f3ca7ab9269d89dcd4f
Nguyen Huy Hoang <ngxhuyhoang@gmail.com>
2023-07-19 18:11:07 +0700

    feat: remove husky

--- src/common/based.entity.ts
+++ src/common/based.entity.ts
@@ -1,4 +1,9 @@
-import { CreateDateColumn, Index, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class BasedEntity {
   @PrimaryGeneratedColumn()
