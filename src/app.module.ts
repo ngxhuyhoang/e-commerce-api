43079a41216cefac07ccceab94fc90eb1eb553a8
Hoang Nguyen Huy <ngxhuyhoang@gmail.com>
2023-08-01 23:15:13 +0700

    feat: update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from '@databases/type-orm-config.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env.development', '.env.production', '.env.staging'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useClass: TypeOrmConfigService,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder().setTitle('E Commerce API Docs').setVersion('1.0').addBearerAuth().build();
-
-  // console.log('Haha');
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  const port = configService.get('PORT');
+  const env = configService.get('NODE_ENV');
+
   await app.listen(3000, () => {
-    console.info('Server is running on http://localhost:3000');
-    console.info('Swagger docs http://localhost:3000/docs');
+    console.info(`Environment: ${env}`);
+    console.info(`Server is running on http://localhost:${port}}`);
+    console.info(`Swagger docs http://localhost:${port}/docs`);
   });
 }
 bootstrap();
