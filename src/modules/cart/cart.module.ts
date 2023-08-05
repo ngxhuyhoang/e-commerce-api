bfdc045c74bed6ac87667f6bf1c9e86f540da966
Nguyen Huy Hoang <ngxhuyhoang@gmail.com>
2023-08-05 17:16:37 +0700

    feat: update

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -4,9 +4,10 @@ import { CartController } from './cart.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { AccountEntity } from '@modules/account/entities/account.entity';
+import { ProductEntity } from '@modules/product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CartEntity, AccountEntity])],
+  imports: [TypeOrmModule.forFeature([CartEntity, AccountEntity, ProductEntity])],
   controllers: [CartController],
   providers: [CartService],
 })
